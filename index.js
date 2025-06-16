c31fe65dc6d67b80f8ed9dd96fd4178653f930b0
Ezekiel Leslie Omire <ezekielomire@gmail.com>
2025-06-16 16:24:17 +0300

    Adds required scope lab functions and global variables

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Write your solution in this file!
+// Declare customerName in global scope
+var customerName = 'bob';
+
+// Uppercase customerName
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+// Set bestCustomer in global scope
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+// Overwrite bestCustomer
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+
+// Declare leastFavoriteCustomer as constant
+const leastFavoriteCustomer = 'initial customer';
+
+// Attempt to change constant (will throw an error)
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'new customer';
+}
+
